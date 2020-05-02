b0dc6461c5168f95d7a2328656743acc17e6e8a3
pragya004 <pragyakumari004@gmail.com>
2020-05-02 21:03:28 +0530

    lorem ipsum

--- modal.js
+++ modal.js
@@ -1,5 +1,4 @@
 var modal=document.getElementById("modal");
-var updateHabitButton = document.getElementById("update-habit-button");
 
 var closeModalButton = document.getElementById("habit-button");
 var openModalButton = document.getElementById("add-modal");
@@ -8,7 +7,6 @@ var openModalButton = document.getElementById("add-modal");
 //Listening for click
 openModalButton.addEventListener("click",openModal);
 closeModalButton.addEventListener("click",closeModal);
-updateHabitButton.addEventListener("click",changeBackground);
 
 //modal functions
 function openModal()
@@ -19,9 +17,4 @@ function openModal()
 function closeModal()
 {
     modal.style.display='none';
-}
-
-function changeBackground()
-{
-    // document.body.style.backgroundColor="#9d9d9d";
 }
